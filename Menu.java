44c8a4f4b28348365546d3cc9955f86d12956ec0
andrey <tritg@yandex.ru>
2023-08-10 08:53:27 +0300

    change menu

--- Menu.java
+++ Menu.java
@@ -7,12 +7,12 @@ public class Menu {
         Object elAnimal;
         int start = 1;
         int end = 7;
+        Scanner in = new Scanner(System.in);
         while (true) {
             System.out.println(
                     "МЕНЮ\n1. Добавить новое животное\n2. Определить класс животного по индексу\n3. Посмотреть список команд конкретнного животного по индексу\n4. Показать всех животных с полной информацией о них\n5. Обучить животное новой команде\n6. Сохранить животных в файл\n7. Выход из программы");
             System.out.println("Введите нужный пункт меню:");
 
-            Scanner in = new Scanner(System.in);
             start = 1;
             end = 7;
             String inputUser = in.nextLine();
@@ -100,19 +100,21 @@ public class Menu {
                     arr.showAll();
                     break;
                 case 5:
-                    System.out.print("Введите индекс нужного животного в диапазоне от " + start +
-                            " до " + end + ": ");
-                    start = 0;
+                    // System.out.print("Введите индекс нужного животного в диапазоне от " + start +
+                    // " до " + end + ": ");
+                    // start = 0;
+                    // end = arr.getSizeArr();
+                    // inputUser = in.nextLine();
+                    // idx = ValidationInput(inputUser, start, end);
                     end = arr.getSizeArr();
-                    inputUser = in.nextLine();
-                    idx = ValidationInput(inputUser, start, end);
+                    idx = userInputIdxAnimal(end);
                     elAnimal = arr.getElemet(idx);
                     System.out.print("Введите новую команду животному");
                     inputUser = in.nextLine();
                     ((Animals) elAnimal).setNewCommands(inputUser);
                     break;
                 case 6:
-                    // arr.voiceAll();
+
                     break;
                 case 7:
                     break;
@@ -156,6 +158,16 @@ public class Menu {
         } else {
             return false;
         }
+    }
+
+    public static int userInputIdxAnimal(int end) {
+
+        System.out.println("Введите индекс нужного животного в диапазоне от " + start + " до " + end + ": ");
+        int start = 0;
+        Scanner in = new Scanner(System.in);
+        String inputUser = in.nextLine();
+        int idx = ValidationInput(inputUser, start, end);
+        return start;
     }
 
 }
